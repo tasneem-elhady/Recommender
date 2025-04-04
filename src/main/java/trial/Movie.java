779d2072c10d6f22b7c4382a2638715fbb617f94
Tasneem <2000036@eng.asu.edu.eg>
2025-04-04 16:18:08 +0200

    basic movie structure

--- src/main/java/trial/Movie.java
+++ src/main/java/trial/Movie.java
@@ -1,4 +1,25 @@
 package trial;
 
 public class Movie {
+    public String[] getGenre() {
+        return genre;
+    }
+
+    public String title;
+    public String id;
+    public String []genre;
+
+    public Movie(String title, String id, String[] genre) {
+        this.title = title;
+        this.id = id;
+        this.genre = genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
