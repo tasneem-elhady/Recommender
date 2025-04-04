1c576f7395ad2b205b005688cfd35310cada4b10
Tasneem <2000036@eng.asu.edu.eg>
2025-04-04 16:18:24 +0200

    basic user structure

--- src/main/java/trial/User.java
+++ src/main/java/trial/User.java
@@ -1,4 +1,18 @@
 package trial;
 
 public class User {
+    public String name;
+    public String id;
+    public String [] likedMovies;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String[] getLikedMovies() {
+        return likedMovies;
+    }
 }
