7ec34e7253281199a5d90e7259c219d7c127448b
Mariam Hesham <99722575+MariamH78@users.noreply.github.com>
2025-04-12 21:00:45 +0200

    added movie title validation unit tests

--- src/test/java/trial/MovieValidator/MovieTitleValidatorTest.java
+++ src/test/java/trial/MovieValidator/MovieTitleValidatorTest.java
@@ -1,22 +1,138 @@
 package trial.MovieValidator;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import trial.Exceptions.DuplicateException;
-import trial.Exceptions.InvalidFileFormatException;
-import trial.MovieValidator.MovieValidator;
-import trial.Parser.UserParser;
-import trial.User;
-import trial.UserValidator.UserValidator;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.eq;
+import trial.Exceptions.InvalidMovieTitleException;
 
 public class MovieTitleValidatorTest {
+    @Test 
+    void testNoIssuesWithMovieTitle() {
+        String movieTitle = "Se7en";
+        assertDoesNotThrow(() ->{
+            MovieTitleValidator.validate(movieTitle);
+        });    
+    }
     
+    @Test 
+    void testOneWordNotCapitalized() {
+        String movieTitle = "se7en";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        }); 
+    }
+    
+    @Test
+    void testOneWordStartsWithNumber() {
+        String movieTitle = "7esen";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        }); 
+    }
+    
+        @Test 
+    void testTwoWordsCap() {
+        String movieTitle = "Se7en Movie";
+        assertDoesNotThrow(() -> {
+            MovieTitleValidator.validate(movieTitle);
+        });    
+    }
+    
+    @Test
+    void testTwoWordsNoCapFirst() {
+        String movieTitle = "se7en Movie";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+    
+    @Test
+    void testTwoWordsNoCapSecond() {
+        String movieTitle = "Se7en movie";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+    
+    @Test
+    void testTwoWordsNumberFirst() {
+        String movieTitle = "7esen Movie";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+    
+    @Test
+    void testTwoWordsNumberSecond() {
+        String movieTitle = "Se7en 7ovie";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    
+    @Test
+    void testMultipleWordsNoIssues() {
+        String movieTitle = "The Shaw5hank Redemption";
+        assertDoesNotThrow(() ->{
+            MovieTitleValidator.validate(movieTitle);
+        }); 
+    }
+    
+    @Test
+    void testMultipleWordsNoCapMiddle() {
+        String movieTitle = "The shaw5hank Redepmption";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    @Test
+    void testMultipleWordsNoCapFirst() {
+        String movieTitle = "the Shaw5hank Redemption";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    @Test
+    void testMultipleWordsNoCapLast() {
+        String movieTitle = "The Shaw5hank redemption";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    @Test
+    void testMultipleWordsNumberMiddle() {
+        String movieTitle = "The 5hawshank Redemption";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    @Test
+    void testMultipleWordsNumberFirst() {
+        String movieTitle = "7he Shawshank Redemption";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    @Test
+    void testMultipleWordsNumberLast() {
+        String movieTitle = "The Shawshank 5edemption";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
+
+    @Test
+    void testMultipleWordsNoCapAndNumbers() {
+        String movieTitle = "The quick 5rown fox3";
+        assertThrows(InvalidMovieTitleException.class, () -> {
+            MovieTitleValidator.validate(movieTitle);
+        });
+    }
 }
