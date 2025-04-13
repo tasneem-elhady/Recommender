34e64fb27a8ed4bee75b7468108b29785c058c90
Mariam Hesham <99722575+MariamH78@users.noreply.github.com>
2025-04-13 12:51:29 +0200

    Added movie ID validation tests

--- src/test/java/trial/MovieValidator/MovieIdValidatorTest.java
+++ src/test/java/trial/MovieValidator/MovieIdValidatorTest.java
@@ -1,22 +1,87 @@
 package trial.MovieValidator;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import trial.Exceptions.DuplicateException;
-import trial.Exceptions.InvalidFileFormatException;
-import trial.MovieValidator.MovieValidator;
-import trial.Parser.UserParser;
-import trial.User;
-import trial.UserValidator.UserValidator;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.eq;
+import trial.Exceptions.InvalidMovieIdLettersException;
+import trial.Exceptions.InvalidMovieIdNumbersException;
 
 public class MovieIdValidatorTest {
+    @Test 
+    void testNoIssuesWithMovieId() {
+        String movieTitle = "Se7en", movieId = "S001";
+        assertDoesNotThrow(() ->{
+            MovieIdValidator.validate(movieTitle, movieId);
+        });    
+    }
     
+    @Test 
+    void testTooFewLettersInMovieId() {
+        String movieTitle = "Se7en", movieId = "001";
+        assertThrows(InvalidMovieIdLettersException.class, () ->{
+            MovieIdValidator.validate(movieTitle, movieId);
+        }); 
+    }
+    
+    @Test 
+    void testTooFewNumbersInMovieId() {
+        String movieTitle = "Se7en", movieId = "S01";
+        assertThrows(InvalidMovieIdNumbersException.class, () ->{
+            MovieIdValidator.validate(movieTitle, movieId);
+        }); 
+    }
+    @Test 
+    void testTooManyNumbersInMovieId() {
+        String movieTitle = "Se7en", movieId = "S0001";
+        assertThrows(InvalidMovieIdLettersException.class, () ->{
+            MovieIdValidator.validate(movieTitle, movieId);
+        }); 
+    }
+    @Test 
+    void testTooManyLettersInMovieId() {
+        String movieTitle = "Se7en", movieId = "SS001";
+        assertThrows(InvalidMovieIdLettersException.class, () ->{
+            MovieIdValidator.validate(movieTitle, movieId);
+        }); 
+    }
+    
+    @Test 
+    void testLowercaseLettersInMovieId() {
+        String[] movieTitles = {"Se7en", "The Shawshank Redemption", "The Shawshank Redemption", "The Shawshank Redemption", "The Shawshank Redemption"};
+        String[] movieIds = {"s001", "tSR001", "TsR001", "TSr001", "tSr001"};
+
+        for (int i = 0; i < movieTitles.length; i++) {
+            String title = movieTitles[i];
+            String id = movieIds[i];
+            assertThrows(InvalidMovieIdLettersException.class, () -> {
+                MovieIdValidator.validate(title, id);
+            }); 
+        }
+    }
+    @Test 
+    void testNumbersInMovieId() {
+        String[] movieTitles = {"Se7en", "The Shawshank Redemption", "The Shawshank Redemption", "The Shawshank Redemption", "The Shawshank Redemption"};
+        String[] movieIds = {"0001", "0SR001", "T0R001", "TS0001", "0S0001"};
+
+        for (int i = 0; i < movieTitles.length; i++) {
+            String title = movieTitles[i];
+            String id = movieIds[i];
+            assertThrows(InvalidMovieIdLettersException.class, () -> {
+                MovieIdValidator.validate(title, id);
+            }); 
+        }
+    }
+    
+    @Test 
+    void testWrongLettersInMovieId() {
+        String[] movieTitles = {"Se7en", "The Shawshank Redemption", "The Shawshank Redemption", "The Shawshank Redemption", "The Shawshank Redemption"};
+        String[] movieIds = {"X001", "XSR001", "TXR001", "TSX001", "XSX001"};
+
+        for (int i = 0; i < movieTitles.length; i++) {
+            String title = movieTitles[i];
+            String id = movieIds[i];
+            assertThrows(InvalidMovieIdLettersException.class, () -> {
+                MovieIdValidator.validate(title, id);
+            }); 
+        }
+    }
 }
